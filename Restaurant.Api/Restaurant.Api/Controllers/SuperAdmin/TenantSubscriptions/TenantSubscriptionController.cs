c16df7b99988106dcbcf0d1bd96d65d7e2ce9f5e
Adith <adithravindranofficial@gmail.com>
2026-05-05 22:42:59 +0530

    Created controller

--- Restaurant.Api/Restaurant.Api/Controllers/SuperAdmin/TenantSubscriptions/TenantSubscriptionController.cs
+++ Restaurant.Api/Restaurant.Api/Controllers/SuperAdmin/TenantSubscriptions/TenantSubscriptionController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Restaurant.Application.Common;
 using Restaurant.Application.SuperAdmin.DTOs;
 using Restaurant.Application.SuperAdmin.Interfaces.TenantSubscriptionManagement.GetTenantSubscription;
+using Restaurant.Application.SuperAdmin.Interfaces.TenantSubscriptionManagement.AssignSubscription;
 
 namespace Restaurant.Api.Controllers.SuperAdmin.TenantSubscriptions
 {
@@ -12,16 +13,28 @@ namespace Restaurant.Api.Controllers.SuperAdmin.TenantSubscriptions
     public class TenantSubscriptionController : ControllerBase
     {
         private readonly IGetTenantSubscriptionService _getTenantSubscriptionService;
+        private readonly IAssignSubscriptionService _assignSubscriptionService;
 
-        public TenantSubscriptionController(IGetTenantSubscriptionService getTenantSubscriptionService)
+        public TenantSubscriptionController(
+            IGetTenantSubscriptionService getTenantSubscriptionService,
+            IAssignSubscriptionService assignSubscriptionService)
         {
             _getTenantSubscriptionService = getTenantSubscriptionService;
+            _assignSubscriptionService = assignSubscriptionService;
         }
 
         [HttpGet("{id}/subscription")]
         public async Task<ActionResult<ApiResponse<TenantSubscriptionDto>>> GetTenantSubscription(int id)
         {
             var result = await _getTenantSubscriptionService.GetTenantSubscriptionAsync(id);
+
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPost("{id}/subscription")]
+        public async Task<ActionResult<ApiResponse<TenantSubscriptionDto>>> AssignSubscription(int id, [FromBody] AssignSubscriptionDto dto)
+        {
+            var result = await _assignSubscriptionService.AssignSubscriptionAsync(id, dto);
 
             return StatusCode(result.StatusCode, result);
         }
